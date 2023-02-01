f548d006e6c0f58a469ad67916a7157cf841711f
Karina Titov <ktitov@joor.com>
2023-02-01 00:12:53 -0600

    fix: fix

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(host="172.17.0.2", port=5000, debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
