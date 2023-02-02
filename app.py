769f30014df4b587cb289d9706152b8a6d6881b6
Karina Titov <ktitov@joor.com>
2023-02-02 17:16:41 -0600

    fix: enable debug

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
